dfff07d3302fc586b9e7db4e9a6dd22a9af312df
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-05-29 10:09:51 +0530

    Update urluploader.py

--- DaisyX/modules/urluploader.py
+++ DaisyX/modules/urluploader.py
@@ -92,6 +92,6 @@ ETA: {}""".format(
 __help__ = """
  • `/up`*:* reply to a direct download link to upload it to telegram as files
  
-© @ImJanindu 🇱🇰"""
+© @piroXpower 🇮🇳"""
 
 __mod_name__ = "URL Upload 📤"
